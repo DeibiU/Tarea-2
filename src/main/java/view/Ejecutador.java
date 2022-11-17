c0fcf483ec03b594ac8d017c29dd729d4bb25808
david <dhuertasl@ucenfotec.ac.cr>
2022-11-16 21:00:43 -0600

    Correccion/Actualizacion del Ejecutador

--- src/main/java/view/Ejecutador.java
+++ src/main/java/view/Ejecutador.java
@@ -1,4 +1,4 @@
-package com.example.tarea2;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Ejecutador extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Ejecutador.class.getResource("Menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 405, 318);
+        stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
     }
